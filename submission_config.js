f320e7ca018c626f4e47a438fb8df70f531356fe
fengfeng <2026ntustcell1002@gmail.com>
2026-05-13 23:12:46 +0800

    Update Apps Script API URL

--- submission_config.js
+++ submission_config.js
@@ -1,5 +1,5 @@
 window.HEFC_SUBMISSION_API_URL =
-'https://script.google.com/macros/s/AKfycbw0xkNiFHh3nAl1C8BtqruIUQ_CQXNtih5bPWYpt28tNuG5N7NWHX6d-SIEzCbxzWU-wg/exec';
+'https://script.google.com/macros/s/AKfycbwwk7FZy5KhUkyeOgvNEcR1jVnOZUzxQYD9NStcr76c9hqp3D-pkp9u0WuYVy7OZ9z5/exec';
 
 window.HEFC_GOOGLE_CLIENT_ID =
 '521873320707-kmujg3oqnipuce98gmldhbav024oeddg.apps.googleusercontent.com';
