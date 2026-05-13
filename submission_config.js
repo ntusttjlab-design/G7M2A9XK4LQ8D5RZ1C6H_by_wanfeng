115c3492ccc54602573a0e58181674420b0474fe
fengfeng <2026ntustcell1002@gmail.com>
2026-05-13 22:51:00 +0800

    Publish HEFC 2026 html0512 site

--- submission_config.js
+++ submission_config.js
@@ -1,3 +1,5 @@
 window.HEFC_SUBMISSION_API_URL =
-'https://script.google.com/macros/s/AKfycbwlNnwn92YiJEMLrdhSij6g464TTjjBKknrHnzXoBOoVbqf2K2TY-gHO9SgfNvw7Rt4PQ/exec';
+'https://script.google.com/macros/s/AKfycbw0xkNiFHh3nAl1C8BtqruIUQ_CQXNtih5bPWYpt28tNuG5N7NWHX6d-SIEzCbxzWU-wg/exec';
 
+window.HEFC_GOOGLE_CLIENT_ID =
+'521873320707-kmujg3oqnipuce98gmldhbav024oeddg.apps.googleusercontent.com';
