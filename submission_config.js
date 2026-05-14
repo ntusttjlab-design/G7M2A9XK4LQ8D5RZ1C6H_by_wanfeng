23b7ada66c706ddecb6f0501351ada1c6546d1a8
豐豐 <fengfeng@fengfengdeMacBook-Air.local>
2026-05-14 17:09:53 +0800

    Update staff allowlist backend for hchuanggood

--- submission_config.js
+++ submission_config.js
@@ -1,5 +1,5 @@
 window.HEFC_SUBMISSION_API_URL =
-'https://script.google.com/macros/s/AKfycbwXeaTSyDsuWNHqAyguxIniWR8ssi_0Z6W4HPKsYEEWOTkk-UjRxfgnKGkU-Eq6-taE9A/exec';
+'https://script.google.com/macros/s/AKfycbyr-FLGGDT0MTbcXFMWyMq-9xM5RU44MNjDr-MWTu_P2bW4DwV6tWddJ-dXY9RGnvmSQQ/exec';
 
 window.HEFC_GOOGLE_CLIENT_ID =
 '521873320707-kmujg3oqnipuce98gmldhbav024oeddg.apps.googleusercontent.com';
