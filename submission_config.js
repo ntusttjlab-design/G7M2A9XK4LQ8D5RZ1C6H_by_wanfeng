e44de80512254d52d86a85a73eaa844c8e8356d5
豐豐 <fengfeng@fengfengdeMacBook-Air.local>
2026-05-14 15:31:25 +0800

    Update staff allowlist backend URL

--- submission_config.js
+++ submission_config.js
@@ -1,5 +1,5 @@
 window.HEFC_SUBMISSION_API_URL =
-'https://script.google.com/macros/s/AKfycbwwk7FZy5KhUkyeOgvNEcR1jVnOZUzxQYD9NStcr76c9hqp3D-pkp9u0WuYVy7OZ9z5/exec';
+'https://script.google.com/macros/s/AKfycbwXeaTSyDsuWNHqAyguxIniWR8ssi_0Z6W4HPKsYEEWOTkk-UjRxfgnKGkU-Eq6-taE9A/exec';
 
 window.HEFC_GOOGLE_CLIENT_ID =
 '521873320707-kmujg3oqnipuce98gmldhbav024oeddg.apps.googleusercontent.com';
