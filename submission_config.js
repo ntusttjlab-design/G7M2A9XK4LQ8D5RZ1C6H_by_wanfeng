f0b6d37fd581d8b62c9430a242eea4d8d1bc5c9b
豐豐 <fengfeng@fengfengdeMacBook-Air.local>
2026-05-24 05:21:56 +0800

    Use split timestamp backend

--- submission_config.js
+++ submission_config.js
@@ -1,5 +1,5 @@
 window.HEFC_SUBMISSION_API_URL =
-'https://script.google.com/macros/s/AKfycbyr-FLGGDT0MTbcXFMWyMq-9xM5RU44MNjDr-MWTu_P2bW4DwV6tWddJ-dXY9RGnvmSQQ/exec';
+'https://script.google.com/macros/s/AKfycbwFwY4KjCwlGTdrsDxH6h_1mayJvtbnosXrHbn1ebkotUZmeCiWE2yhalhPx9JTdcU0eA/exec';
 
 window.HEFC_GOOGLE_CLIENT_ID =
 '521873320707-kmujg3oqnipuce98gmldhbav024oeddg.apps.googleusercontent.com';
